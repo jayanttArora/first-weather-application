bea92dc3e5603961503b828556d6f6126cd88c95
masteroFluorine <xsjx15@gmail.com>
2021-02-26 15:47:59 +0530

    Switch to local modules

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some helpful text.',
+        helpText: 'Type the name of the location to search weather for.',
         title: 'Help',
         name: 'Jayant Arora'
     })
