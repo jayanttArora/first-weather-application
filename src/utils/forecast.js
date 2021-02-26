61ab8fb8d0a811c7bff2e2512803db461de3ce45
masteroFluorine <xsjx15@gmail.com>
2021-02-26 15:03:30 +0530

    Add humidity to forecast.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find weather for the given location. Try another search !!')
         } else {
-            callback(undefined, `${body.current.weather_descriptions}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`)
+            callback(undefined, `${body.current.weather_descriptions}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The Humidity is ${body.current.humidity} %.`)
         }
     })
 }
